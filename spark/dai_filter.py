e8c0a00fc30333b4c2c8d01bb14e6d52a2bc4fcc
Jeffrey Milewski <jeffreymilewski@Jeffreys-MacBook-Pro.local>
2020-06-23 10:31:57 -0400

    update dai filter

--- spark/dai_filter.py
+++ spark/dai_filter.py
@@ -4,6 +4,11 @@ import pyspark.sql.functions as f
 from pyspark.sql.types import ArrayType, StringType
 from pyspark.sql import SparkSession
 
+PG_USER = config('PG_USER')
+PG_DBNAME = config('PG_DBNAME')
+PG_PASSWORD = config('PG_PASSWORD')
+PG_SERVER = config('PG_SERVER')
+
 spark = SparkSession \
     .builder \
     .appName("Python Spark SQL basic example") \
@@ -12,15 +17,13 @@ spark = SparkSession \
 
 df = spark.read \
     .format("jdbc") \
-    .option("url", "jdbc:postgresql://10.0.0.5:5432/postgres") \
+    .option("url", f"jdbc:postgresql://{PG_SERVER}/{PG_DBNAME}") \
     .option("dbtable", "token_transfers_test") \
-    .option("user", "db_select") \
-    .option("password", "<setpassword>") \
+    .option("user", PG_USER) \
+    .option("password", PG_PASSWORD) \
     .option("driver", "org.postgresql.Driver") \
     .load()
 
-df_dai = df.filter(df["token_address"]=="0x6b175474e89094c44da98b954eedeac495271d0f")
-
-df_dai.count()
-
-dai_value_transferred = df_dai.groupBy("token_address").sum().collect()[0][1]
+def dai_sum_by_block():
+    df_dai = df.filter(df["token_address"]=="0x6b175474e89094c44da98b954eedeac495271d0f")
+    df_dai_by_block = df_dai.select("block_number","value").groupBy("block_number").sum("value")
